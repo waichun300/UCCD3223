f52dfa23852fb4586a1898b06c7cd583f24d63c1
waichun300 <wwchun0727@gmail.com>
2024-03-09 01:28:58 +0800

    add variations into questions

--- app/src/main/java/my/edu/utar/practicalassignment/CompareExerciseActivity.java
+++ app/src/main/java/my/edu/utar/practicalassignment/CompareExerciseActivity.java
@@ -20,40 +20,39 @@ import java.util.Random;
 public class CompareExerciseActivity extends AppCompatActivity {
     private int num1;
     private int num2;
+    private int question;
+    private TextView compareTextView;
+    private TextView num1textView;
+    private  TextView num2textView;
+
+    private Button num1Button;
+
+    private Button num2Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare_exercise);
 
-        TextView num1textView = findViewById(R.id.number1textView);
-        TextView num2textView = findViewById(R.id.number2textView);
-        Button num1Button = findViewById(R.id.number1button);
-        Button num2Button = findViewById(R.id.number2button);
+        compareTextView = findViewById(R.id.textView2);
+        num1textView = findViewById(R.id.number1textView);
+        num2textView = findViewById(R.id.number2textView);
+        num1Button= findViewById(R.id.number1button);
+        num2Button = findViewById(R.id.number2button);
 
         Random random = new Random();
-
-
-        do {
-            num1 = random.nextInt(100);
-            num2 = random.nextInt(100);
-        } while (num1 == num2);
-
-
-        num1textView.setText(String.valueOf(num1));
-        num2textView.setText(String.valueOf(num2));
-
-        num1Button.setText(String.valueOf(num1) + " is greater");
-        num2Button.setText(String.valueOf(num2) + " is greater");
+        question = random.nextInt(2);
+        compareTextView.setText(question == 0 ? "Which one is greater?" : "Which one is smaller?");
+        generateNumbers();
 
 
         num1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (num1 > num2) {
-                    displayDialogBox("Correct", num1, num2);
+                    displayDialogBox("Correct. " + num1 +" is greater than " + num2);
                 } else {
-                    displayDialogBox("Incorrect", num2, num1);
+                    displayDialogBox("Incorrect. " + num1 +" is smaller than " + num2);
                 }
             }
         });
@@ -61,23 +60,37 @@ public class CompareExerciseActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (num2 > num1) {
-                    displayDialogBox("Correct", num2, num1);
+                    displayDialogBox("Correct. " + num2 +" is greater than " + num1);
                 } else {
-                    displayDialogBox("Incorrect", num1, num2);
+                    displayDialogBox("Incorrect. " + num2 +" is smaller than " + num1);
                 }
             }
         });
     }
 
-    private void displayDialogBox(String message, int greaterNum, int smallerNum) {
+    private void generateNumbers(){
+        Random random = new Random();
+        do {
+            num1 = random.nextInt(100);
+            num2 = random.nextInt(100);
+        } while (num1 == num2);
+
+        num1textView.setText(String.valueOf(num1));
+        num2textView.setText(String.valueOf(num2));
+
+        num1Button.setText(question == 0 ? String.valueOf(num1) + " is greater" : String.valueOf(num1) + " is smaller");
+        num2Button.setText(question == 0 ? String.valueOf(num2) + " is greater" : String.valueOf(num2) + " is smaller");
+    }
+
+    private void displayDialogBox(String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Answer");
-        builder.setMessage(message + ", " + greaterNum + " is greater than " + smallerNum);
+        builder.setMessage(message);
         builder.setCancelable(false);
         builder.setPositiveButton("Next Question", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                recreate();
+                generateNumbers();
             }
         });
 
